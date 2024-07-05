64132654cc95d8adc3648e62a8d46652a3b632b8
Niklas Ek <niklas.ek@northvolt.com>
2024-07-05 23:31:05 +0200

    [style] remove LESS, use modern CSS

--- lingon.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var lingon        = require('lingon');
-var autoprefixer  = require('gulp-autoprefixer');
-
-// cross browser css. adds vendor prefixes as needed. https://github.com/sindresorhus/gulp-autoprefixer
-lingon.postProcessors.push('less', function() {
-  return autoprefixer();
-});
